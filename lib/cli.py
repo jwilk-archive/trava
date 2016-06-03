1c0173075d52c56f484f0503152f9045b42250a0
Jakub Wilk <jwilk@jwilk.net>
2016-06-03 19:01:01 +0200

    Add --raw-cr.
    Clean CRs by default.
    Use --raw-cr to disable this behavior.
    This option replaces (badly named) --clean-lf.

--- lib/cli.py
+++ lib/cli.py
@@ -64,7 +64,7 @@ def dispatch(regex):
 
 def main():
     ap = argparse.ArgumentParser()
-    ap.add_argument('--clean-lf', action='store_true')
+    ap.add_argument('--raw-cr', action='store_true')
     ap.add_argument('url', metavar='URL')
     options = ap.parse_args()
     url = urllib.parse.urljoin('https://travis-ci.org/', options.url)
@@ -157,13 +157,13 @@ def show_job(options, project, job_id):
     url = 'https://api.travis-ci.org/jobs/{id}/log.txt'
     url = url.format(id=job_id)
     with get(url) as fp:
-        if options.clean_lf:
+        if options.raw_cr:
+            shutil.copyfileobj(fp, sys.stdout.buffer)
+        else:
             for line in fp:
                 line = line.rstrip(b'\r\n').rsplit(b'\r', 1)[-1]
                 sys.stdout.buffer.write(line)
                 sys.stdout.buffer.write(b'\n')
-        else:
-            shutil.copyfileobj(fp, sys.stdout.buffer)
 
 __all__ = ['main']
 
